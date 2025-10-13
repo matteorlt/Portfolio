69072f003bb65f814b4dc210610a0785ceec70cf
Matteo <matteo.rannou-letexier@askoria.eu>
2025-10-13 16:58:52 +0200

    fix: deploy dupli

--- client/src/pages/Quote.jsx
+++ client/src/pages/Quote.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import React, { useState, useEffect, useRef } from 'react';
 let BackgroundConstellation = null;
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
