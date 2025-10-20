3182fd36058ba78795e53e4bdc478953a4dad2f0
matteorlt <matteo.rannouletexier2025@campus-eni.fr>
2025-10-20 03:35:37 +0200

    Add Vercel Functions for email with CORS support + EmailJS alternative

--- api/contact.js
+++ api/contact.js
@@ -2,6 +2,17 @@
 const nodemailer = require('nodemailer');
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
